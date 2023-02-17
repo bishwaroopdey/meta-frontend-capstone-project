27841e694103b0b1c552c22e23fa4e2b00fe1105
Bishwaroop <bishwaroop.dey@gmail.com>
2023-02-17 12:16:43 +0530

    Added the logo and navigation elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import Nav from "./Nav";
+import primaryLogo from "../assets/images/primary logo.png";
 
 function Header() {
     return (
       <header className="header">
+        <img src={primaryLogo} alt="" className="primary-logo"/>
         <Nav/>
       </header>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,26 @@
 function Nav() {
     return (
       <nav className="navbar">
+        <ul className="navbar-list">
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">Home</a>
+            </li>
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">About</a>
+            </li>
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">Menu</a>
+            </li>
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">Reservations</a>
+            </li>
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">Order Online</a>
+            </li>
+            <li className="navbar-list-item">
+                <a href="" className="navbar-list-item-link">Login</a>
+            </li>
+        </ul>
       </nav>
     );
   }
