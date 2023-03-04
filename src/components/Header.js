426b7a8608fef70a5c31711a462e44d52eaa72be
Bishwaroop <bishwaroop.dey@gmail.com>
2023-03-04 23:19:07 +0530

    Stylized the nav bar and made it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
+import React, { useState } from 'react';
 import Nav from "./Nav";
+import "../assets/styles/Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import primaryLogo from "../assets/images/primary logo.png";
 
 function Header() {
-    return (
-      <header className="header">
-        <img src={primaryLogo} alt="" className="primary-logo"/>
-        <Nav/>
-      </header>
-    );
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+    console.log(isActive);
   }
+
+  return (
+    <header className="header">
+      <img src={primaryLogo} alt="primary-logo" className="primary-logo"/>
+      <button className="navbar-toggle" onClick={toggle}>
+        <FontAwesomeIcon icon={faBars} size="2x"/>
+      </button>
+      <Nav isActive={isActive} />
+    </header>
+  );
+}
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,30 @@
-function Nav() {
+import "../assets/styles/Nav.css"
+
+function Nav(props) {
     return (
-      <nav className="navbar">
-        <ul className="navbar-list">
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">Home</a>
-            </li>
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">About</a>
-            </li>
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">Menu</a>
-            </li>
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">Reservations</a>
-            </li>
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">Order Online</a>
-            </li>
-            <li className="navbar-list-item">
-                <a href="" className="navbar-list-item-link">Login</a>
-            </li>
-        </ul>
+        <nav className={`navbar ${props.isActive? "open" : ""}`}>
+            <ul className="navbar-list">
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">Home</a>
+                </li>
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">About</a>
+                </li>
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">Menu</a>
+                </li>
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">Reservations</a>
+                </li>
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">Order Online</a>
+                </li>
+                <li className="navbar-list-item">
+                    <a href="" className="navbar-list-item-link">Login</a>
+                </li>
+            </ul>
       </nav>
     );
-  }
+}
 
 export default Nav;
