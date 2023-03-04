04f28a33c67cbc5e3c362a3d837ac00ddc4bfd58
Bishwaroop <bishwaroop.dey@gmail.com>
2023-03-05 01:57:26 +0530

    Created app components and added navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,27 @@
-import "../assets/styles/Nav.css"
+import "../assets/styles/Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav(props) {
     return (
         <nav className={`navbar ${props.isActive? "open" : ""}`}>
             <ul className="navbar-list">
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">Home</a>
+                    <Link to="/" className="navbar-list-item-link">Home</Link>
                 </li>
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">About</a>
+                    <Link to="/about" className="navbar-list-item-link">About</Link>
                 </li>
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">Menu</a>
+                    <Link to="/menu" className="navbar-list-item-link">Menu</Link>
                 </li>
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">Reservations</a>
+                    <Link to="/reservations" className="navbar-list-item-link">Reservations</Link>
                 </li>
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">Order Online</a>
+                    <Link to="/order-online" className="navbar-list-item-link">Order Online</Link>
                 </li>
                 <li className="navbar-list-item">
-                    <a href="" className="navbar-list-item-link">Login</a>
+                    <Link to="/login" className="navbar-list-item-link">Login</Link>
                 </li>
             </ul>
       </nav>
